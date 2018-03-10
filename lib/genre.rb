34aedb409a194e345dda830c2b0f5676001fff11
Marin Cabac <marin.cabac@gmail.com>
2018-03-10 21:56:12 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,19 +1,12 @@
 class Genre
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-  def songs
-    @songs
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def artists
-    @songs.map {|s| s.artist}
-      end
+  attr_accessor :name, :songs
+def initialize(name)
+  @name=name 
+  @songs=[]
+  
 end
+def add_songs(s)
+  @songs<<s
+  s.artist=self
+end 
+end 
