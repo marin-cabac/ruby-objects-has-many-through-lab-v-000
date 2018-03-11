fa0be912c9ea3ddd1f14c4ce78961f639e277c74
Marin Cabac <marin.cabac@gmail.com>
2018-03-11 16:50:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
   attr_accessor :name, :genre, :artist
   def initialize(name,genre)
     @name=name
-    @genre=genre
-    genre.add_song(self)
-  end
-end
+    @genre = genre 
+    genre.add_song=self
+  end 
+end 
