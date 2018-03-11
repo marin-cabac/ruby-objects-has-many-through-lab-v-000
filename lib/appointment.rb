558b45c9e0f73fa27eb6a0ea91c98b757f651ceb
Marin Cabac <marin.cabac@gmail.com>
2018-03-11 18:53:29 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,13 +4,5 @@ class Appointment
     @date=date
     @doctor=doc
     @doctor.add_appointment(self)
-  end 
-end 
-    
-
-
-
-An appointment should be initialized with a date (as a string, like `"Monday, August 1st"`), and a doctor. 
-The `#initialize` method should tell the new appointment that it belongs to the doctor passed in as an argument 
-and tell the doctor that it now has this appointment in its collection of appointments. 
-Use the `Doctor#add_appointment` method here. 
+  end
+end
