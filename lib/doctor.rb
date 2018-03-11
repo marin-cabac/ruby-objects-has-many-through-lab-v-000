9e06ab6225624b602bb9b63ab7881f8c3392ed68
Marin Cabac <marin.cabac@gmail.com>
2018-03-11 18:46:28 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,15 @@
+class Doctor
+attr_accessor :name
+def initialize(name)
+  @name=name
+  @appointments=[]
+end 
+def appointments
+  @appointments
+end
+
+def patients
+  @appointments.map{|x|x.patient}
+end 
+end 
+
